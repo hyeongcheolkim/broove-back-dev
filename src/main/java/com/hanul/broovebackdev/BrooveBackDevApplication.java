d199a2d8962621f15fbc07a7286c34b903c9b569
hyeongcheolkim <tori1753@naver.com>
2023-07-29 23:58:19 +0900

    Initial commit

---
+++ src/main/java/com/hanul/broovebackdev/BrooveBackDevApplication.java
@@ -0,0 +1,13 @@
+package com.hanul.broovebackdev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrooveBackDevApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BrooveBackDevApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hanul/broovebackdev/BrooveBackDevApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hanul.broovebackdev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrooveBackDevApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
